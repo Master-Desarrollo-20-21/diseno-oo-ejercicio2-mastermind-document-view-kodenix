1420dbdc34c7bf6154abb7bb52813f439bfaf97a
Alberto Morales Fernandez <kodenix@gmail.com>
2020-12-05 20:48:06 +0100

    Copy current code and initialized architecture diagram

---
+++ project/src/Result.java
@@ -0,0 +1,20 @@
+
+public class Result {
+	
+	private int blacks;
+	private int whites;
+	
+	Result(int whites, int blacks) {
+		this.blacks = blacks;
+        this.whites = whites;
+	}
+	
+	public int getWhites() {
+		return whites;
+	}
+	
+	public int getBlacks() {
+		return blacks;
+	}
+
+}
